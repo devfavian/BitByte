cbdde6e40ff4d94a2b95a0663de09b5b6e15adb1
devfavian <dev.favian@gmail.com>
2025-08-11 18:19:39 +0200

    Improved /avatar design and layout.

--- src/usercommands/AvatarCommand.java
+++ src/usercommands/AvatarCommand.java
@@ -2,6 +2,7 @@ package usercommands;
 import utils.Command;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
@@ -9,17 +10,23 @@ public class AvatarCommand implements Command {
 	@Override
 	public void handle(SlashCommandInteractionEvent event) {
 		
-		EmbedBuilder embed = new EmbedBuilder();
-		embed.setTitle("User avatar");
-		
 		User target = event.getOption("user") != null
 				? event.getOption("user").getAsUser()
 				: event.getUser();
 		
+		Member membro = event.getOption("user") != null
+				? event.getOption("user").getAsMember()
+				: event.getMember();
 		
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setColor(new java.awt.Color(0x5865F2));
+		embed.setTimestamp(java.time.Instant.now());
+		embed.setDescription("📷 " + membro.getAsMention());
 		String avatar = target.getEffectiveAvatarUrl() + "?size=1024";
 		embed.setImage(avatar);
+		embed.setFooter("ByteBot • Avatar");
 		event.replyEmbeds(embed.build()).queue();
+		
 		}
 	}
 
