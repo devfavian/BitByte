ff365afea7d5de9653d1f3885b2d7e083ed74ae0
devfavian <dev.favian@gmail.com>
2025-08-11 18:00:32 +0200

    Improved /help design and layout.

--- src/usercommands/HelpCommand.java
+++ src/usercommands/HelpCommand.java
@@ -2,21 +2,56 @@ package usercommands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import utils.Command;
 
 public class HelpCommand implements Command {
 	@Override
 	public void handle(SlashCommandInteractionEvent event) {
-		EmbedBuilder embed = new EmbedBuilder();
-		embed.setTitle("Command List");
-		embed.addField("/Bhelp", "Displays a list of all available commands\n", false);
-		embed.addField("/Bping", "Responds with 'pong'\n", false);
-		embed.addField("/Buserinfo", "Shows information about yourself or a mentioned user\n", false);
-		embed.addField("/Bclear <number_of_messages>", "Deletes a specified number of messages in a channel\n", false);
-		embed.addField("/Bavatar <mention>", "Shows the avatar of yourself or a mentioned user in high resolution\n", false);
-		embed.addField("/Bservericon", "Show the server icon", false);
-		embed.addField("/Bremind <seconds> <message>", "!Bremind <seconds> <message>\n", false);
 		
-		event.replyEmbeds(embed.build()).queue();
+		final String invite = "https://discord.com/oauth2/authorize?client_id=1403010360847630336&permissions=8&integration_type=0&scope=applications.commands+bot";
+		final String github = "https://github.com/devfavian";
+		final String discord = "https://discord.gg/QtUzEyZERv";
+		
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setAuthor("ByteBot • Help", null, null);
+		embed.setTitle("📖 Command List");
+		embed.setDescription("Use `/command` with suggested options. Start typing to see autocomplete.");
+		embed.setColor(new java.awt.Color(0x5865F2));
+		embed.setTimestamp(java.time.Instant.now());
+		embed.setFooter("ByteBot • Help");
+
+		
+		
+		embed.addBlankField(false);
+		
+		//Utilities
+		embed.addField("🧰 **Utilities**", "\u200B" , false);
+		embed.addField("**/help**", "Displays a list of all available commands\n", true);
+		embed.addField("**/ping**", "Responds with 'pong'\n", true);
+		embed.addField("**/userinfo**", "Shows information about yourself or a mentioned user\n", true);
+		embed.addField("**/avatar <mention>**", "Shows the avatar of yourself or a mentioned user in high resolution\n", true);
+		embed.addField("**/servericon**", "Show the server icon", true);
+		embed.addField("**/remind <seconds> <message>**", "Schedules a reminder and pings you when it’s due\n", true);
+		embed.addBlankField(false);
+		
+		
+		embed.addField("🛡️ **Moderation**", "\u200B", false);
+		embed.addField("**/clear <number_of_messages>**", "Deletes a specified number of messages in a channel\n", true);
+		embed.addBlankField(false);
+		
+		
+		embed.addField("👋 **Welcome**", "\u200B", false);
+		embed.addField("**welcome message**", "Sends a welcome message to new members\n", true);
+		embed.addField("**auto-role**", "Automatically assigns one or multiple roles to new members\n", true);
+		embed.addBlankField(false);
+		
+		event.replyEmbeds(embed.build()).addActionRow(
+					Button.link(invite, "➕ Invite ByteBot"),
+					Button.link(github, "📦 GitHub"),
+					Button.link(discord, "🕹️ My discord")
+				).queue();
+		
+		
 	}
 }
